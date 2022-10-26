9b3b2e2a24c739d127f78f2e8e6a9623fb898ff6
fundDeveloper22 <openfund365@gmail.com>
2022-10-26 14:57:35 +0900

    update deploy script

---
+++ scripts/01-deploy-token.ts
@@ -0,0 +1,20 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { ethers } from "hardhat"
+
+async function main() {
+  const [test_account_1] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", test_account_1.address);
+  console.log("Account balance:", (await test_account_1.getBalance()).toString());
+
+  const XXXToken = await ethers.getContractFactory("XXXToken");
+  const token = await XXXToken.deploy();
+  await token.deployed();
+  console.log("XXXToken address : ", token.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
